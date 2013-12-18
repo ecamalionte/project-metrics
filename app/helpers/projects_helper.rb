b0da029c4952fcd17dda8597bd97f1122a7956d7
Eric Camalionte <eric80hz@yahoo.com.br>
2013-12-18 03:17:18 -0200

    adding progress bar to project|s list

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,17 @@
 module ProjectsHelper
+  def calculate_spent_time(project)
+    if project.dead_line && project.started_on
+      total = project.dead_line - project.started_on
+      current = Date.today - project.started_on
+      spent = ((current/total).to_f * 100).to_i
+    end
+  end
+
+  def detect_alerts(project)
+    spent_time = calculate_spent_time(project)
+    return if project.status.blank? && spent_time.blank?
+    return 'progress-success'  if project.status >= spent_time
+    return 'progress-warning' if project.status < spent_time && spent_time <= 50
+    return 'progress-danger'  if project.status < spent_time && spent_time > 50
+  end
 end
