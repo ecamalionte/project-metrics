ba27e097a624e79bab54f2d112c216eb1ce16d9b
Eric Camalionte <eric80hz@yahoo.com.br>
2013-12-19 00:17:24 -0200

    refactoring in project_helper to extract important business logic to model

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,17 +1,9 @@
 module ProjectsHelper
-  def calculate_spent_time(project)
-    if project.dead_line && project.started_on
-      total = project.dead_line - project.started_on
-      current = Date.today - project.started_on
-      spent = ((current/total).to_f * 100).to_i
-    end
-  end
 
   def detect_alerts(project)
-    spent_time = calculate_spent_time(project)
-    return if project.status.blank? && spent_time.blank?
-    return 'progress-success'  if project.status >= spent_time
-    return 'progress-warning' if project.status < spent_time && spent_time <= 50
-    return 'progress-danger'  if project.status < spent_time && spent_time > 50
+    return if project.blank?
+    return 'progress-success' if project.progress_success?
+    return 'progress-warning' if project.progress_warning?
+    return 'progress-danger'  if project.progress_danger?
   end
 end
