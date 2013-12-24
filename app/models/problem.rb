7fa328310de32be501e1affbc6bccdd07b21f23d
Eric Camalionte <eric80hz@yahoo.com.br>
2013-12-23 23:27:04 -0200

    adding comments to Problems

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,2 +1,3 @@
 class Problem < ActiveRecord::Base
+  has_many :comments, as: :commentable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Community::Application.routes.draw do
     resources :comments
   end
 
-  resources :problems
+  resources :problems do
+    resources :comments
+  end
 
   root "dashboard#index"
 end
