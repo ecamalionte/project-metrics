0a297b707579cbb003f170e92dcf154889478191
Bruno Bacarini <bruno.bacarini@locaweb.com.br>
2014-01-02 17:37:21 -0200

    add validation to roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ActiveRecord::Base
     
   validates :email, uniqueness: true
   validates :email, :password, :role, presence: true
+  validates :role, :inclusion => { :in => %w(admin member), :message => "%{value} is not a valid role" }
 end
