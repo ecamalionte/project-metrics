fd327ef3c86e833a90a917d0ce1809680f95b40e
Bruno Bacarini <bruno.bacarini@locaweb.com.br>
2014-01-02 18:26:10 -0200

    add page of notice

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,10 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :read, :all
+    if user.admin? 
+      can :manage, :all
+    else
+      can :read, :all
+    end
   end
 end
