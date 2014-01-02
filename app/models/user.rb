0a7d8ce8f53465317f929eec4cc8fa2a05f0915c
Bruno Bacarini <bruno.bacarini@locaweb.com.br>
2014-01-02 18:07:52 -0200

    add method and test to check user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ActiveRecord::Base
   validates :email, uniqueness: true
   validates :email, :password, :role, presence: true
   validates :role, :inclusion => { :in => %w(admin member), :message => "%{value} is not a valid role" }
+
+  def admin?
+    self.role == "admin"
+  end
+
+  def member?
+    self.role == "member"
+  end
 end
