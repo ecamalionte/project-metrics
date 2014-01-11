01d83391047b865c9d9095aa40c24b7f6b2a4205
Bruno Bacarini <bacarini.bruno@gmail.com>
2014-01-11 13:52:20 -0200

    add relation on votes and comments when we create them

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,68 +4,52 @@ class CommentsController < ApplicationController
   before_action :load_commentable
   before_action :authorize, except: [:index, :show]
 
-  # GET /comments
-  # GET /comments.json
   def index
     @comments = @commentable.comments
   end
 
-  # GET /comments/1
-  # GET /comments/1.json
   def show
     @comment = @commentable.comments.find(params[:id])
   end
 
-  # GET /comments/new
   def new
     @comment = @commentable.comments.new
   end
 
-  # GET /comments/1/edit
   def edit
     @comment = @commentable.comments.find(params[:id])
   end
 
-  # POST /comments
-  # POST /comments.json
   def create
     @comment = @commentable.comments.new(comment_params)
+    @comment.user_id = @current_user.id
 
     respond_to do |format|
       if @comment.save
         format.html { redirect_to build_path_comment(@comment), notice: 'Comment was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @comment }
       else
         format.html { render action: 'new' }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /comments/1
-  # PATCH/PUT /comments/1.json
   def update
     @comment = @commentable.comments.find(params[:id])
 
     respond_to do |format|
       if @comment.update(comment_params)
         format.html { redirect_to build_path_comment(@comment), notice: 'Comment was successfully updated.' }
-        format.json { head :no_content }
       else
         format.html { render action: 'edit' }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /comments/1
-  # DELETE /comments/1.json
   def destroy
     @comment = @commentable.comments.find(params[:id])
     @comment.destroy
     respond_to do |format|
       format.html { redirect_to build_path_comments }
-      format.json { head :no_content }
     end
   end
 
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,68 +4,52 @@ class VotesController < ApplicationController
   before_action :load_votable
   before_action :authorize, except: [:index, :show]
 
-  # GET /votes
-  # GET /votes.json
   def index
     @votes = @votable.votes
   end
 
-  # GET /votes/1
-  # GET /votes/1.json
   def show
     @vote = @votable.votes.find(params[:id])
   end
 
-  # GET /votes/new
   def new
     @vote = @votable.votes.new
   end
 
-  # GET /votes/1/edit
   def edit
     @vote = @votable.votes.find(params[:id])
   end
 
-  # POST /votes
-  # POST /votes.json
   def create
     @vote = @votable.votes.new(vote_params)
+    @vote.group_id = @current_user.group_id
 
     respond_to do |format|
       if @vote.save
         format.html { redirect_to build_path_vote(@vote), notice: 'Vote was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @vote }
       else
         format.html { render action: 'new' }
-        format.json { render json: @vote.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /votes/1
-  # PATCH/PUT /votes/1.json
   def update
     @vote = @votable.votes.find(params[:id])
 
     respond_to do |format|
       if @vote.update(vote_params)
         format.html { redirect_to build_path_vote(@vote), notice: 'Vote was successfully updated.' }
-        format.json { head :no_content }
       else
         format.html { render action: 'edit' }
-        format.json { render json: @vote.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /votes/1
-  # DELETE /votes/1.json
   def destroy
     @vote = @votable.votes.find(params[:id])
     @vote.destroy
     respond_to do |format|
       format.html { redirect_to build_path_votes }
-      format.json { head :no_content }
     end
   end
 
