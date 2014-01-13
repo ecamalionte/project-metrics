5b94525162f1a05286e0442b10fa4163965efa80
Eric Camalionte <eric80hz@yahoo.com.br>
2014-01-13 01:04:11 -0200

    skipping navigation to let the user vote fast

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -17,9 +17,24 @@ module VotesHelper
     send("#{votable_name}_path", @votable)
   end
 
+  def build_root_path_for_votable
+    votable_name = @votable.class.name.underscore
+    send("#{votable_name}s_path")
+  end
+
   def build_path_votes
     votable_name = @votable.class.name.underscore
     send("#{votable_name}_votes_path")
+  end
+
+  def build_path_new_vote_for(votable)
+    votable_name = votable.class.name.underscore
+    send("new_#{votable_name}_vote_path", "#{votable_name}_id" => votable.id)
+  end
+
+  def build_path_edit_vote_for(votable, vote)
+    votable_name = votable.class.name.underscore
+    send("edit_#{votable_name}_vote_path", "#{votable_name}_id" => votable.id, id: vote.id)
   end
 
   def build_path_new_vote
