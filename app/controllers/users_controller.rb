9a41b8a83b7c71dfa434abea345d56f840975dc9
Eric Camalionte <eric80hz@yahoo.com.br>
2014-02-04 01:00:30 -0200

    better user.s crud

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
-  skip_before_action :redirect_to_dashboard, only: [:new, :create] 
-  before_action :set_user, only: :create
+  skip_before_action :redirect_to_dashboard, only: [:new, :create]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   load_and_authorize_resource only: :edit
+
+  def index
+    @users = User.all
+  end
 
   def new
     @user = User.new(invitation_token: params[:invitation_token])
@@ -14,6 +18,7 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
       redirect_to root_url, notice: t("messages.success")
@@ -26,12 +31,33 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to projects_url }
+      format.json { head :no_content }
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: t("messages.success") }
+      else
+        format.html {
+          flash[:error] =  t("messages.error")
+          render 'edit'
+        }
+      end
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:email, :username, :password, :password_confirmation, :role_id, :group_id, :invitation_id)
   end
 
   def set_user
-    @user = User.new(user_params)
+    @user = User.find(params[:id])
   end
 end
