e83e2b175192a165b9e50acef383862b74138969
Bruno Bacarini <bruno.bacarini@locaweb.com.br>
2014-02-14 17:48:02 -0200

    add config on production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,16 @@ Community::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            'community.notifier@gmail.com',
+    password:             'bacarinieric',
+    authentication:       'plain',
+    enable_starttls_auto: true 
+  }
 end
