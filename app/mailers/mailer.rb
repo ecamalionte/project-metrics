60cfd7fadafe0f67381ec4626899e04b0411c93f
Bruno Bacarini <bruno.bacarini@locaweb.com.br>
2014-02-17 11:16:50 -0300

    improve signup email text

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -7,7 +7,6 @@ class Mailer < ActionMailer::Base
     @signup_url = signup_url
 
     invitation.update_attribute(:sent_at, Time.now)
-    mail(to: @recipient,
-         subject: "[Signed up] Welcome #{@recipient}")
+    mail(to: @recipient, subject: "Convite Cadastramento Community")
   end
 end
